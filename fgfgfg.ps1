28da914b91b86e85b7a0dcc1284791e1b951d80b
Admin <Admin>
2020-10-01 01:04:48 +0300

    Updating via git sync.

--- fgfgfg.ps1
+++ fgfgfg.ps1
@@ -1,9 +1,1 @@
-﻿New-UDDashboard -Title "Hello, World!" -Content {
-    New-UDTypography -Text "Hello, World!"
-
-    New-UDButton -Text "Learn more about Universal Dashboard" -OnClick {
-        Invoke-UDRedirect https://docs.ironmansoftware.com
-    }
-}
-
-# demo
+﻿Demo content
