971bd29db2b2c82fe214104fae25d6d431c6ac35
Admin <Admin>
2020-10-01 01:03:08 +0300

    Updating via git sync.

--- .universal/dashboards.ps1
+++ .universal/dashboards.ps1
@@ -1,3 +1,4 @@
-﻿New-PSUDashboard -Name "qa" -FilePath "qa.ps1" -BaseUrl "/qa" -Framework "UniversalDashboard:3.1.0" -DisableAutoStart 
+﻿New-PSUDashboard -Name "demo" -FilePath "demo.ps1" -BaseUrl "/dashboard" -Framework "UniversalDashboard:3.1.0" 
+New-PSUDashboard -Name "qa" -FilePath "qa.ps1" -BaseUrl "/qa" -Framework "UniversalDashboard:3.1.0" -DisableAutoStart 
 New-PSUDashboard -Name "Test" -FilePath "Test.ps1" -BaseUrl "/test" -Framework "UniversalDashboard:3.1.0" 
-New-PSUDashboard -Name "demo" -FilePath "demo.ps1" -BaseUrl "/dashboard" -Framework "UniversalDashboard:3.1.0"
+New-PSUDashboard -Name "fgfgfg" -FilePath "fgfgfg.ps1" -BaseUrl "/gfgfgfgfg" -Framework "UniversalDashboard:3.1.0"
